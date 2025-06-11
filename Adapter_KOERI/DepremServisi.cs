2bf0ec5dce9fd8c89d4937ddb24910287f2833dc
aribilgiogr <aribilgiogr@gmail.com>
2025-06-11 14:36:09 +0300

    Add project files.

---
+++ Adapter_KOERI/DepremServisi.cs
@@ -0,0 +1,22 @@
+﻿namespace Adapter_KOERI
+{
+    public class DepremServisi
+    {
+        private readonly HttpClient httpClient;
+
+        public DepremServisi(IHttpClientFactory factory)
+        {
+            httpClient = factory.CreateClient();
+        }
+
+        public async Task<IEnumerable<DepremModel>> DepremleriGetirAsync()
+        {
+            string url = "http://www.koeri.boun.edu.tr/scripts/sondepremler.asp";
+            string html = await httpClient.GetStringAsync(url);
+
+            var depremler = new List<DepremModel>();
+
+            return depremler;
+        }
+    }
+}
