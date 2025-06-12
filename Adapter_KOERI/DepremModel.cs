66221cc877e5228af71b273b2c1f40673bd769c9
aribilgiogr <aribilgiogr@gmail.com>
2025-06-12 10:51:05 +0300

    KOERI Service added

--- Adapter_KOERI/DepremModel.cs
+++ Adapter_KOERI/DepremModel.cs
@@ -6,7 +6,7 @@
         public double Enlem { get; set; } // Enlem (N)
         public double Boylam { get; set; } // Boylam (E)
         public double Derinlik { get; set; } // Derinlik (km)
-        public double ML { get; set; } // ML
+        public double Siddet { get; set; } // ML
         public string Yer { get; set; } = null!; // Yer
     }
 }
--- Adapter_KOERI/Program.cs
+++ Adapter_KOERI/Program.cs
@@ -1,9 +1,15 @@
+using Adapter_KOERI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton<DepremServisi>();
 
 var app = builder.Build();
 
@@ -16,6 +22,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => new { message = "Hello World!" });
+app.MapGet("/", async (DepremServisi servis) =>
+{
+    try
+    {
+        var depremler = await servis.DepremleriGetirAsync();
+        return Results.Json(depremler);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 
 app.Run();
