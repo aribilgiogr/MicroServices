337b210218459702cce588cf32039470a4e035ee
aribilgiogr <aribilgiogr@gmail.com>
2025-06-12 12:15:42 +0300

    TCMB Service added

---
+++ Adapter_TCMB/Program.cs
@@ -0,0 +1,31 @@
+using Adapter_TCMB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton<CurrencyService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", async (CurrencyService currencyService) =>
+{
+    var currencies = await currencyService.GetCurrenciesAsync();
+    return Results.Json(currencies);
+});
+
+app.Run();
